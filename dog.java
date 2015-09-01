482aa8b06a0a12e55cb4590bedea59730cb175d2
Bo Yuan Zheng <aquadracon@gmail.com>
2015-08-31 21:43:02 -0400

    Added "testMethods" method to "Dog" class.

--- dog.java
+++ dog.java
@@ -1,5 +1,11 @@
 // This class represents a dog object.
 class Dog {
+	
+	//Runs all methods
+	public testMethods() {
+		run();
+	}
+	
 	public run() {
 		system.out.print("The dog runs around in circles");
 	}
